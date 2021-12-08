831ca5d97bfaa7430fb301835d9dabb4c74d140b
Diêgo Nunes <diego.nunes@deliverymuch.com.br>
2021-12-08 12:05:17 +0000

    create layouts with state

--- app/src/main/java/br/dnassuncao/composestate/MainActivity.kt
+++ app/src/main/java/br/dnassuncao/composestate/MainActivity.kt
@@ -3,41 +3,80 @@ package br.dnassuncao.composestate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import br.dnassuncao.composestate.ui.theme.ComposeStateTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeStateTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            BoxScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BoxScreen() {
+
+    val boxSize = 400.dp
+    var count by remember {
+        mutableStateOf(0)
+    }
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .size(boxSize)
+                .background(Color.Red),
+            contentAlignment = Alignment.Center
+        ) {
+            var childSize = boxSize - 20.dp
+            for (i in 0 until count) {
+                Box(
+                    modifier = Modifier
+                        .size(childSize)
+                        .rotate(i * 3f)
+                        .background(Color.Gray)
+                        .border(1.dp, Color.Black)
+                )
+
+                childSize -= 20.dp
+            }
+        }
+
+        OutlinedTextField(value = "${count}", onValueChange = {})
+
+        Row {
+            Button(onClick = { count++ }, modifier = Modifier.padding(8.dp)) {
+                Text(text = "Increase")
+            }
+            Button(
+                onClick = { count = if (count <= 0) 0 else count - 1 },
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text(text = "Decrease")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeStateTheme {
-        Greeting("Android")
-    }
+fun PreviewBoxScreen() {
+    BoxScreen()
 }
